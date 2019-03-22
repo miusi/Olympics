119677bb3291e486bfe48bcdc9cbe94dbb0eb817
Godric <miaochenGod@outlook.com>
2019-03-22 11:10:01 +0800

    移除logincontroller，增加accountcontroller

--- Olympics.BaseApi/Controllers/AccountController.cs
+++ Olympics.BaseApi/Controllers/AccountController.cs
@@ -5,27 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Olympics.Interfaces;
+using Olypmics.Common;
 using Orleans;
 
 namespace Olympics.BaseApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class LoginController : ControllerBase
+    public class AccountController : ControllerBase
     {
         private IClusterClient client;
 
-        public LoginController(IClusterClient client)
+        public AccountController(IClusterClient client)
         {
             this.client = client;
         }
 
-        [HttpGet]
-        public ActionResult Login(string username, string password)
+        [HttpGet("login")]
+        public async Task<IActionResult> Login(string username, string password)
         {
-            var userBiz = client.GetGrain<IUserBiz>(username);
-            userBiz.Login(username, password);
-            return Ok();
+            IUserBiz userBiz = client.GetGrain<IUserBiz>(username);
+            BaseResult baseResult = await userBiz.Login(username, password);
+            return Ok(baseResult);
         }
     }
 }
